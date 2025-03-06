2e2bd0baa4923aeca44ea337f661acb04da4fa14
minggle0322 <minggle0322@gmail.com>
2025-03-06 20:49:25 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import "./App.css";
+import Home from "./pages/main"; // Home 컴포넌트 불러오기
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>이제부터 시작해보는 React 페이지 구성하기</h1>
-      <p>
-        React로 배워보는 UX/UI 디자인 실력 향상
-      </p>
+    <div>
+      <Home />  {/* Home 컴포넌트 표시 */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>메인 페이지입니다! 🏠</h1>
+      <p>React 앱의 첫 화면입니다.</p>
+    </div>
+  );
+};
+
+export default Home;

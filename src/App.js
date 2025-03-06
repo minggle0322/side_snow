f764c2c090faa9fd2f43393f488399c49c28f706
minggle0322 <minggle0322@gmail.com>
2025-03-06 20:38:22 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>이제부터 시작해보는 React 페이지 구성하기</h1>
+      <p>
+        React로 배워보는 UX/UI 디자인 실력 향상
+      </p>
+    </div>
+  );
+}
+
+export default App;

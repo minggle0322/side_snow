cfb6712d0c6308c2d1f2837c32c7c79d380e874f
Jayden Park <minggle72@gmail.com>
2025-03-09 20:42:00 +0900

    backend test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/main";
 import Header from "./pages/header";
@@ -6,6 +7,13 @@ import Signup from "./pages/signup";
 import Login from "./pages/login";
 
 function App() {
+  const [hello, setHello] = useState('')
+
+  useEffect(() => {
+      axios.get('/api/hello')
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error))
+  }, []);
 
 
 
@@ -18,6 +26,7 @@ function App() {
           <Route path="/signup" element={<Signup/>} />
           <Route path="/login" element={<Login/>} />
         </Routes>
+        백엔드에서 가져온 데이터입니다 : {hello}
        </div>
     </Router>
    );

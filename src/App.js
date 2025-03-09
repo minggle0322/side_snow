111cd336a143631d391f37e39fd7df5a76cce4f4
Jayden Park <minggle72@gmail.com>
2025-03-09 20:53:44 +0900

    backend connect moved to app.js -> main.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/main";
 import Header from "./pages/header";
@@ -7,15 +6,6 @@ import Signup from "./pages/signup";
 import Login from "./pages/login";
 
 function App() {
-  const [hello, setHello] = useState('')
-
-  useEffect(() => {
-      axios.get('/api/hello')
-      .then(response => setHello(response.data))
-      .catch(error => console.log(error))
-  }, []);
-
-
 
    return (
     <Router>
@@ -26,7 +16,6 @@ function App() {
           <Route path="/signup" element={<Signup/>} />
           <Route path="/login" element={<Login/>} />
         </Routes>
-        백엔드에서 가져온 데이터입니다 : {hello}
        </div>
     </Router>
    );
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';  
 
-const Home = () => {
-  return (
+function Home() {
+  const [hello, setHello] = useState('')
+
+   useEffect(() => {
+       axios.get('/api/hello')
+       .then(response => setHello(response.data))
+       .catch(error => console.log(error))
+   }, []);
+
+   return (
     <div>
       <h1>메인 페이지입니다! 🏠</h1>
       <p>React 앱의 첫 화면입니다.</p>
+      백엔드에서 가져온 데이터입니다 : {hello}
     </div>
   );
 };

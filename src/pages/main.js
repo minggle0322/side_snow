7b4d9a2f0249666f68cec6a20f8b809ce8e80355
Jayden Park <minggle72@gmail.com>
2025-04-09 21:51:30 +0900

    # change to fake address

--- src/pages/main.js
+++ src/pages/main.js
@@ -79,9 +79,9 @@ function Home() {
         <div className="footer-inner">
           <p>&copy; 2025 화이트밸런스. 모든 권리 보유.</p>
           <div className="footer-links">
-            <a href="#">이용약관</a>
-            <a href="#">개인정보처리방침</a>
-            <a href="#">문의하기</a>
+            <a href="/terms">이용약관</a>
+            <a href="/policy">개인정보처리방침</a>
+            <a href="/inquire">문의하기</a>
           </div>
         </div>
       </footer>
